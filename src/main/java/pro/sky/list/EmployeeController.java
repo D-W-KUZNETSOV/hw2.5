5aac0db929eb1c5065b3370d99579451118373f5
Dmitryi Kuznetsov <d.w.kuznetsov@yandex.ru>
2024-09-11 14:13:28 +0300

    list

--- src/main/java/pro/sky/list/EmployeeController.java
+++ src/main/java/pro/sky/list/EmployeeController.java
@@ -2,10 +2,12 @@ package pro.sky.list;
 
 
 import java.util.Collection;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pro.sky.list.exeption.ValidationExeption;
 
 @RestController
 @RequestMapping("/Employee")
@@ -24,24 +26,41 @@ public class EmployeeController {
       @RequestParam String lastName,
       @RequestParam int departmentId,
       @RequestParam int salary) {
-    return service.addEmployee(firstName, lastName, departmentId, salary);
+    validate(firstName,lastName);
+
+    return service.addEmployee(StringUtils.capitalize(firstName),
+        StringUtils.capitalize(lastName),
+        departmentId,
+        salary);
   }
 
   @GetMapping("/remove")
   public String removeEmployee(@RequestParam String firstName,
       @RequestParam String lastName) {
-    return service.removeEmployee(firstName, lastName);
+    return service.removeEmployee(StringUtils.capitalize(firstName),
+        StringUtils.capitalize(lastName));
   }
 
   @GetMapping("/find")
   public Employee findEmployee(@RequestParam String firstName,
       @RequestParam String lastName) {
-    return service.findeEmployee(firstName, lastName);
+    return service.findeEmployee(StringUtils.capitalize(firstName),
+        StringUtils.capitalize(lastName));
   }
 
   @GetMapping("/find-all")
   public Collection<Employee> findAll() {
     return service.findAll();
+  }
+
+  public void validate(String ...values) {
+    for (String value : values) {
+      if (!StringUtils.isAlpha(value)) {
+        throw new ValidationExeption();
+      }
+
+    }
+
   }
 }
 
